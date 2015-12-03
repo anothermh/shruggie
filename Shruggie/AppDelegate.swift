dd0f618cc10df77eace44002756edacadeb99f95
Mitch Eaton <meaton@mitchs-mbp.us.ad.westfield.com>
2015-12-03 11:35:13 -0800

    Initial Commit

---
+++ Shruggie/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Shruggie
+//
+//  Created by Mitch Eaton on 12/3/15.
+//  Copyright (c) 2015 Mitch Eaton. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
