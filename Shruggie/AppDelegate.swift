f0881a79e41e9ce65607963c5e71723cc6c658e0
anothermh <michael@immediatelyapp.com>
2015-12-11 18:51:46 -0800

    Upgrade to latest Swift version; add Retina icon

--- Shruggie/AppDelegate.swift
+++ Shruggie/AppDelegate.swift
@@ -11,17 +11,42 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+
+    @IBOutlet weak var Shrugged: NSMenu!
+    
+    @IBAction func addItem(sender: NSMenuItem) {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        pasteboard.clearContents()
+        pasteboard.setString("ಠ_ಠ", forType: NSPasteboardTypeString)
+    }
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
+    @IBAction func Shruggie(sender: NSMenuItem) {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        pasteboard.clearContents()
+        pasteboard.setString("¯\\_(ツ)_/¯", forType: NSPasteboardTypeString)
+
+    }
+    @IBAction func Table(sender: NSMenuItem) {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        pasteboard.clearContents()
+        pasteboard.setString("(╯°□°）╯︵ ┻━┻", forType: NSPasteboardTypeString)
+    }
     @IBOutlet weak var window: NSWindow!
 
+    @IBAction func Exit(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "shruggieIcon")
+        icon!.template = true
+        statusItem.image = icon
+        statusItem.menu = Shrugged
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
+   
 
 }
 
